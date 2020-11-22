6a037db6495ca06dc99e4fd96348321ecf2f3da0
Anna Pronina <kiisya1@yandex.ru>
2020-11-22 21:31:20 +0300

    Убирает комментарии в vendor.js

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -45,40 +45,6 @@
       return str;
     }
   }
-/*
-  export default (form) => {
-    if (!form) {
-        throw Error('Target form element missing for function `reportValidity`.')
-    }
-
-    if (HTMLFormElement.prototype.reportValidity) {
-        return form.reportValidity();
-    } else {
-        if (form.checkValidity()) {
-            return true;
-        }
-
-        if (!form.reportValidityFakeSubmit) {
-            form.reportValidityFakeSubmit = document.createElement('button');
-            form.reportValidityFakeSubmit.setAttribute('type', 'submit');
-            form.reportValidityFakeSubmit.setAttribute('hidden', 'hidden');
-            form.reportValidityFakeSubmit.setAttribute('style', 'display:none');
-            form.reportValidityFakeSubmit.setAttribute('class', 'reportValidityFakeSubmit');
-
-            form.reportValidityFakeSubmit.addEventListener('click', evt => {
-                if (form.checkValidity()) {
-                    evt.preventDefault();
-                }
-            });
-
-            form.appendChild(form.reportValidityFakeSubmit);
-        }
-
-        form.reportValidityFakeSubmit.click();
-
-        return false;
-    }
-} */
 
 // Полифилл object-fit
   if ('objectFit' in document.documentElement.style === false) {
